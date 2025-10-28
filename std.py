907382dee4215ad3dc4ffcdc1745c41c30ce6bb3
imjoe77 <nathanielbandi636@gmail.com>
2025-10-28 11:53:51 +0530

    Second commit

---
+++ std.py
@@ -0,0 +1,8 @@
+# Collect student details from user input
+name = "Nathaniel"
+clg_name = "BVB"
+
+# Display the collected information
+print(f"College Name: {clg_name}")
+print(f"Student ID: {id}")
+print(f"Student Name: {name}")
